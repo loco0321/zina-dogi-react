58564b9d0b92778ac350724cc6f0ddc68cbb34ee
Cristian Trujillo <cristian.trujillo.ext@nokia.com>
2019-03-11 14:29:29 -0500

    fix: pagination

--- src/UI/Pagination/Pagination.js
+++ src/UI/Pagination/Pagination.js
@@ -1,67 +1,72 @@
 import React from 'react';
-import Icon from '../icon/icon.component'
+import Icon from '../icon/icon.component';
+import { Field } from 'formik'
 
 const paginator = props => {
-    return (
-        <ul className="pagination">
-            <li
-                className="page-item"
-                onClick={() => {
-                    props.previous && props.changePage(props.previous);
-                }}
-            >
-                <a className="page-link">
-                    <Icon
-                        className={btnClass(props.previous)}
-                        icon='chevron-left'
-                    />
-                </a>
-            </li>
-            <li className="page-item input">
-                <input
-                    className="form-control paginator"
-                    type="text"
-                    defaultValue={props.actualPage}
-                    onKeyPress={evt => {
-                        if (evt.key === 'Enter') {
-                            let { value } = document.querySelector(
-                                '.form-control.paginator'
-                            );
-                            if (!value || value === '') {
-                                value = 1;
-                            }
-                            value > props.pages
-                                ? props.changePage(props.pages)
-                                : props.changePage(value);
-                        }
-                    }}
-                />
-            </li>
-            <li className="page-item separator">of</li>
-            <li className="page-item total">{props.pages}</li>
-            <li
-                className="page-item"
-                onClick={() => {
-                    props.next && props.changePage(props.next);
-                }}
-            >
-                <a className="page-link">
-                    <Icon
-                        className={btnClass(props.next)}
-                        icon="chevron-right"
-                    />
-                </a>
-            </li>
-        </ul>
-    );
+  console.log(props);
+
+  return (
+    <ul className="pagination">
+      <li
+        className="page-item"
+        onClick={() => {
+          props.previous && props.changePage(props.previous);
+        }}
+      >
+        <a className="page-link">
+          <Icon
+            className={btnClass(props.previous)}
+            icon='chevron-left'
+          />
+        </a>
+      </li>
+      <li className="page-item input">
+        <React.Fragment>
+          <input
+            className="form-control paginator"
+            type="text"
+            value={props.actualPage}
+            onKeyPress={evt => {
+              if (evt.key === 'Enter') {
+                let { value } = document.querySelector(
+                  '.form-control.paginator'
+                );
+                if (!value || value === '') {
+                  value = 1;
+                }
+                value > props.pages
+                  ? props.changePage(props.pages)
+                  : props.changePage(value);
+              }
+            }}
+          />
+        </React.Fragment>
+      </li>
+      <li className="page-item separator">of</li>
+      <li className="page-item total">{props.pages}</li>
+      <li
+        className="page-item"
+        onClick={() => {
+          props.next && props.changePage(props.next);
+        }}
+      >
+        <a className="page-link">
+          <Icon
+            className={btnClass(props.next)}
+            icon="chevron-right"
+          />
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 const btnClass = value => {
-    let classes = [];
-    if (!value) {
-        classes.push('disabled');
-    }
-    return classes.join(' ');
+  let classes = [];
+  if (!value) {
+    classes.push('disabled');
+  }
+  return classes.join(' ');
 };
 
 export default paginator;
