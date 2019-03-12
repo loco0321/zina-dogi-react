da5b64adf07ce03fc038531a94259d25eaf143df
Cristian Trujillo <cristian.trujillo.ext@nokia.com>
2019-03-11 19:24:41 -0500

    fix: paginator error and styles

--- src/UI/Pagination/Pagination.js
+++ src/UI/Pagination/Pagination.js
@@ -2,64 +2,71 @@ import React from 'react';
 import Icon from '../icon/icon.component';
 import { Field } from 'formik'
 
-const paginator = props => {
-  console.log(props);
-
-  return (
-    <ul className="pagination">
-      <li
-        className="page-item"
-        onClick={() => {
-          props.previous && props.changePage(props.previous);
-        }}
-      >
-        <a className="page-link">
-          <Icon
-            className={btnClass(props.previous)}
-            icon='chevron-left'
-          />
-        </a>
-      </li>
-      <li className="page-item input">
-        <React.Fragment>
-          <input
-            className="form-control paginator"
-            type="text"
-            value={props.actualPage}
-            onKeyPress={evt => {
-              if (evt.key === 'Enter') {
-                let { value } = document.querySelector(
-                  '.form-control.paginator'
-                );
-                if (!value || value === '') {
-                  value = 1;
+class paginator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+  render() {
+    if(this.inputRef.current){
+      this.inputRef.current.value = this.props.actualPage;
+    }
+    return (
+      <ul className="pagination">
+        <li
+          className="page-item"
+          onClick={() => {
+            this.props.previous && this.props.changePage(this.props.previous);
+          }}
+        >
+          <a className="page-link">
+            <Icon
+              className={btnClass(this.props.previous)}
+              icon='chevron-left'
+            />
+          </a>
+        </li>
+        <li className="page-item input">
+          <React.Fragment>
+            <input
+              ref={this.inputRef}
+              className="form-control paginator"
+              type="text"
+              onKeyPress={evt => {
+                if (evt.key === 'Enter') {
+                  let { value } = document.querySelector(
+                    '.form-control.paginator'
+                  );
+                  if (!value || value === '') {
+                    value = 1;
+                  }
+                  value > this.props.pages
+                    ? this.props.changePage(this.props.pages)
+                    : this.props.changePage(value);
                 }
-                value > props.pages
-                  ? props.changePage(props.pages)
-                  : props.changePage(value);
-              }
-            }}
-          />
-        </React.Fragment>
-      </li>
-      <li className="page-item separator">of</li>
-      <li className="page-item total">{props.pages}</li>
-      <li
-        className="page-item"
-        onClick={() => {
-          props.next && props.changePage(props.next);
-        }}
-      >
-        <a className="page-link">
-          <Icon
-            className={btnClass(props.next)}
-            icon="chevron-right"
-          />
-        </a>
-      </li>
-    </ul>
-  );
-};
+              }}
+            />
+          </React.Fragment>
+        </li>
+        <li className="page-item separator">of</li>
+        <li className="page-item total">{this.props.pages}</li>
+        <li
+          className="page-item"
+          onClick={() => {
+            this.props.next && this.props.changePage(this.props.next);
+          }}
+        >
+          <a className="page-link">
+            <Icon
+              className={btnClass(this.props.next)}
+              icon="chevron-right"
+            />
+          </a>
+        </li>
+      </ul>
+    );
+  }
+}
 
 const btnClass = value => {
   let classes = [];
