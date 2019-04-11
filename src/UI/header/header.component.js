34eeb741f773ac17cfef2296afb0b0f7396981e7
Cristian Trujillo <cristian.trujillo.ext@nokia.com>
2019-04-11 16:52:00 -0500

    feat: reder user name if not have first_name and last_name

--- src/UI/header/header.component.js
+++ src/UI/header/header.component.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  Button
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Button
 } from 'reactstrap';
 import Icon from '../icon/icon.component'
 import Notification from '../notification/notification.component';
@@ -16,61 +16,65 @@ import './header.scss';
 // import logo from '../../../assets/imgs/zina-india-logo.png';
 
 const Header = ({
-  onLogout,
-  user,
-  logo,
-  toggleDrawer,
-  extraMenu,
-  notifications = [],
-  noty = true,
-  ...props
+    onLogout,
+    user,
+    logo,
+    toggleDrawer,
+    extraMenu,
+    notifications = [],
+    noty = true,
+    ...props
 }) => {
-  const fullName = user ? `${user.first_name} ${user.last_name}` : '';
-  props.className = ['navbar navbar-dark header', props.className].join(' ');
-  return (
-    <nav {...props}>
-      <div className="navbar-brand">
-        <Link to="/">
-          <img className="logo" src={logo} alt="zina" />
-        </Link>
-        <div className="menu-btn">
-          <Button onClick={toggleDrawer}>
-            <Icon icon="bars" />
-          </Button>
-        </div>
-      </div>
-      <div className="controls">
-        {extraMenu}
-        {noty && (
-          <UncontrolledDropdown setActiveFromChild>
-            <DropdownToggle tag="span">
-              {/* <Button className="btn-circle"> */}
-              <Icon icon="bell" />
-              {/* </Button> */}
-            </DropdownToggle>
-            <NotificationPanel>
-              {notifications.map((notification, i) => (
-                <Notification
-                  key={`notification_${i}`}
-                  img={notification.img}
-                  title={notification.title}
-                  message={notification.message}
-                />
-              ))}
-            </NotificationPanel>
-          </UncontrolledDropdown>
-        )}
-        <UncontrolledDropdown setActiveFromChild className="custom-carets">
-          <DropdownToggle tag="a" caret>
-            <span className="user-name">{fullName}</span>
-            {/* <Button className="btn-circle"><Icon icon="cog" /></Button> */}
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem onClick={onLogout}>Logout</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </div>
-    </nav>
-  );
+    let fullName = 'Zina'
+    if(user){ 
+        if(user.first_name && user.last_name){
+            fullName = `${user.first_name} ${user.last_name}`;
+        }else if (user.username){
+            fullName = user.username;
+        }
+    }
+    props.className = ['navbar navbar-dark header', props.className].join(' ');
+    return (
+        <nav {...props}>
+            <div className="navbar-brand">
+                <Link to="/">
+                    <img className="logo" src={logo} alt="zina" />
+                </Link>
+                <div className="menu-btn">
+                    <Button onClick={toggleDrawer}>
+                        <Icon icon="bars" />
+                    </Button>
+                </div>
+            </div>
+            <div className="controls">
+                {extraMenu}
+                {noty && (
+                    <UncontrolledDropdown setActiveFromChild>
+                        <DropdownToggle tag="span">
+                            <Icon icon="bell" />
+                        </DropdownToggle>
+                        <NotificationPanel>
+                            {notifications.map((notification, i) => (
+                                <Notification
+                                    key={`notification_${i}`}
+                                    img={notification.img}
+                                    title={notification.title}
+                                    message={notification.message}
+                                />
+                            ))}
+                        </NotificationPanel>
+                    </UncontrolledDropdown>
+                )}
+                <UncontrolledDropdown setActiveFromChild className="custom-carets">
+                    <DropdownToggle tag="a" caret>
+                        <span className="user-name">{fullName}</span>
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem onClick={onLogout}>Logout</DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+            </div>
+        </nav>
+    );
 };
 export default Header;
